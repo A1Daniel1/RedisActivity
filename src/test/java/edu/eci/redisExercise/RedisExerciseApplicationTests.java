ca9847e966590d7d4e472833685a13d0fd880050
Daniel Ahumada <danielahumada071@gmail.com>
2026-06-23 13:03:02 -0500

    client java

---
+++ src/test/java/edu/eci/redisExercise/RedisExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.redisExercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
